ce44e798a346263960cc9e81f684f4f0c434afa8
karmaruntime <aashishbhati685@gmail.com>
2026-02-12 21:07:33 +0530

    chore(lib): export StateTransition from crate root

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@ pub mod constants;
 pub mod errors;
 pub mod keys;
 pub mod math;
+pub mod state;
 
 pub use cache::LocalCache;
 pub use errors::{FheError, FheResult};
 pub use keys::{activate_server_key, load_client_key, load_server_key, KeyManager};
 pub use math::FheMath;
+pub use state::StateTransition;
