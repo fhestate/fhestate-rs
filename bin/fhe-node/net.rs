073c4be212a9712facf99b8b65f75a2c1b0a2cf1
Karma Runtime <87428570+KarmaRuntime@users.noreply.github.com>
2026-02-26 02:03:07 +0530

    chore(node): verify RPC filters and discriminator account fetches
    Synchronized task discovery logic with v1.1 protocol.

--- bin/fhe-node/net.rs
+++ bin/fhe-node/net.rs
@@ -2,6 +2,7 @@ use solana_client::rpc_client::RpcClient;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::pubkey::Pubkey;
 use std::error::Error;
+use sha2::Digest;
 
 #[allow(dead_code)]
 pub struct ChainListener {
@@ -32,7 +33,25 @@ impl ChainListener {
         &self,
         program_id: &Pubkey,
     ) -> Result<Vec<(Pubkey, Vec<u8>)>, Box<dyn Error>> {
-        let accounts = self.client.get_program_accounts(program_id)?;
+        use solana_client::rpc_filter::{RpcFilterType, Memcmp};
+        
+        let mut disc_hasher = sha2::Sha256::new();
+        disc_hasher.update(b"account:Task");
+        let disc = &disc_hasher.finalize()[..8];
+
+        let config = solana_client::rpc_config::RpcProgramAccountsConfig {
+            filters: Some(vec![
+                RpcFilterType::Memcmp(Memcmp::new(0, solana_client::rpc_filter::MemcmpEncodedBytes::Bytes(disc.to_vec()))),
+            ]),
+            account_config: solana_client::rpc_config::RpcAccountInfoConfig {
+                commitment: Some(self.client.commitment()),
+                encoding: Some(solana_account_decoder::UiAccountEncoding::Base64),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let accounts = self.client.get_program_accounts_with_config(program_id, config)?;
         Ok(accounts
             .into_iter()
             .map(|(pk, acc)| (pk, acc.data))
@@ -41,5 +60,34 @@ impl ChainListener {
 
     pub fn is_connected(&self) -> bool {
         self.client.get_health().is_ok()
+    }
+
+    pub fn get_state_containers(
+        &self,
+        program_id: &Pubkey,
+    ) -> Result<Vec<(Pubkey, Vec<u8>)>, Box<dyn Error>> {
+        use solana_client::rpc_filter::{RpcFilterType, Memcmp};
+
+        let mut disc_hasher = sha2::Sha256::new();
+        disc_hasher.update(b"account:StateContainer");
+        let disc = &disc_hasher.finalize()[..8];
+
+        let config = solana_client::rpc_config::RpcProgramAccountsConfig {
+            filters: Some(vec![
+                RpcFilterType::Memcmp(Memcmp::new(0, solana_client::rpc_filter::MemcmpEncodedBytes::Bytes(disc.to_vec()))),
+            ]),
+            account_config: solana_client::rpc_config::RpcAccountInfoConfig {
+                commitment: Some(self.client.commitment()),
+                encoding: Some(solana_account_decoder::UiAccountEncoding::Base64),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let accounts = self.client.get_program_accounts_with_config(program_id, config)?;
+        Ok(accounts
+            .into_iter()
+            .map(|(pk, acc)| (pk, acc.data))
+            .collect())
     }
 }
